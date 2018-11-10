cf792cbeab3364a48e97ec79ce01f6545fee5c23
larryjames042 <larryjames042@gmail.com>
2018-11-10 22:34:24 +0630

    edit mainActivity layout

--- app/src/main/java/mirror/co/larry/podz/MainActivity.java
+++ app/src/main/java/mirror/co/larry/podz/MainActivity.java
@@ -5,9 +5,9 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import mirror.co.larry.podz.databinding.ActivityMainBinding;
 
@@ -20,26 +20,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setSupportActionBar(binding.appBar.toolbar);
-
-       binding.navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // default Fragment
+        loadFragment(new DiscoverFragment());
+       binding.bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-
+    // Bottom Navigation Items onClick
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
             switch (item.getItemId()) { 
                 case R.id.navigation_discover:
-                    return true;
+                    fragment = new DiscoverFragment();
+                    break;
                 case R.id.navigation_Subscription:
-                    return true;
+                    fragment = new SearchFragment();
+                    break;
                 case R.id.navigation_Search:
-                    return true;
+                    fragment = new FavFragment();
+                    break;
             }
-            return false;
+            return loadFragment(fragment);
         }
     };
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment!=null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.content_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 
 }
---
+++ app/src/main/java/mirror/co/larry/podz/SearchFragment.java
@@ -0,0 +1,32 @@
+package mirror.co.larry.podz;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchFragment extends Fragment {
+
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = getLayoutInflater().inflate(R.layout.fragment_search, container, false);
+        Toast.makeText(getActivity(), "Search", Toast.LENGTH_SHORT).show();
+        return v;
+    }
+
+}
